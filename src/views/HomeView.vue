c9bb9a3b307e39ec69f48e7a454e67293ef1bc00
Stefan Franke <stefan.franke@vrvoice.de>
2023-09-29 19:34:14 +0200

    new

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,10 @@
       <!---  Header   -->
       <v-row style="max-width: 100%" class=" mx-0">
         <v-col style=" max-width: 90px">
-          <v-img class="logo" src="https://mpu-institut-saar.de/wp-content/uploads/2022/01/cropped-logo-web-transparent.png">
+          <v-img class=" logo" src="https://mpu-institut-saar.de/wp-content/uploads/2022/01/cropped-logo-web-transparent.png">
           </v-img>
         </v-col>
-        <v-col cols="3" class="pl-0 d-flex align-center">
+        <v-col cols="3" class="ml-9 pl-0 d-flex align-center">
           <div>
             <h2 class="mb-n6">
             MPU Vorbereitung
@@ -21,42 +21,45 @@
           </div>
         </v-col>
         <v-col cols="1" class="d-flex align-center">
-          <h4 class="text-white">
+          <h4 class="cursor text-white">
             MPU
           </h4>
         </v-col>
         <v-col cols="1" class="d-flex align-center">
-          <h4 class="text-white">
+          <h4 class="cursor text-white">
             preise
           </h4>
         </v-col>
+
         <v-col cols="1" class="d-flex align-center">
-          <h4 class="text-white">
-            Kontakt
-          </h4>
-        </v-col>
-        <v-col cols="1" class="d-flex align-center">
-          <h4 class="text-white">
+          <h4 class="cursor text-white">
             Impressum
           </h4>
         </v-col>
         <v-col cols="2" class="d-flex align-center">
-          <div class="d-flex align-center justify-center ueber-mich">
+          <div class="cursor d-flex align-center justify-center ueber-mich">
             <h3>
               Über mich
             </h3>
           </div>
         </v-col>
         <v-col cols="2" class="d-flex align-center">
-          <div class="button-rechts">
-
+          <div class="cursor d-flex align-center justify-center  button-rechts">
+             <h3>
+                 Kontakt
+             </h3>
           </div>
         </v-col>
       </v-row>
       <!---  body   -->
       <v-row style="max-width: 100%" class="mt-16 d-flex justify-center mx-0">
-        <v-col class="d-flex justify-center" cols="5">
-            test
+        <v-col  cols="5">
+            <h1 class="text-white">
+                Willkommen beim Institut <br> für MPU-Beratung <br> und Suchtprävention <br> im Saarland.
+            </h1>
+            <p class="text-white mt-10">
+                Ihr Ansprechpartner rund um das Thema Medizinisch-Psychologische Untersuchung (MPU).
+            </p>
         </v-col>
         <v-col class="d-flex justify-center" cols="5">
             <div class="profil-bild">
@@ -67,7 +70,11 @@
             <v-col cols="10">
                 <v-row  style="max-width: 100%" class="footer mx-0">
                     <v-col></v-col>
+                    <v-divider class="border-opacity-100" vertical="true"></v-divider>
                     <v-col></v-col>
+                    <v-divider class="border-opacity-100" vertical="true"></v-divider>
+                    <v-col></v-col>
+                    <v-divider class="border-opacity-100" vertical="true"></v-divider>
                     <v-col></v-col>
                 </v-row>
             </v-col>
@@ -104,6 +111,7 @@ export default ({
   border-radius: 100%;
   background-color: white;
   height: 70px;
+    margin-left: 40px;
   width: 70px;
 }
 .ueber-mich{
@@ -126,7 +134,7 @@ export default ({
 }
 .profil-bild{
   height: 40vh;
-  width: 80%;
+  width: 100%;
   background-image: url("https://mpu-institut-saar.de/wp-content/uploads/2022/06/999543c1-1-2048x1367.jpg");
   background-size: cover;
   border-radius: 70px 70px 70px 200px;
@@ -140,7 +148,11 @@ export default ({
     background-image: linear-gradient(to right, rgba(192, 192, 128, 0.51) 50%, rgba(255, 176, 1, 0.51) 70%);
     backdrop-filter: blur(4px);
     box-shadow: 4px 4px 15px black;
+    padding: 20px;
 
+}
+.cursor{
+    cursor: pointer;
 }
 
 </style>
